eca0d83c1abf6abfc9500a379ef70c2eda24ce34
FKrup <github.8sqeu@simplelogin.com>
2024-06-24 22:34:12 +0200

    feat: :sparkles: added dbus entities dbconnection() functin

---
+++ dbus_entities.py
@@ -0,0 +1,16 @@
+import os
+import dbus
+
+
+class SystemBus(dbus.bus.BusConnection):
+    def __new__(cls):
+        return dbus.bus.BusConnection.__new__(cls, dbus.bus.BusConnection.TYPE_SYSTEM)
+
+
+class SessionBus(dbus.bus.BusConnection):
+    def __new__(cls):
+        return dbus.bus.BusConnection.__new__(cls, dbus.bus.BusConnection.TYPE_SESSION)
+
+
+def dbusconnection():
+    return SessionBus() if "DBUS_SESSION_BUS_ADDRESS" in os.environ else SystemBus()
